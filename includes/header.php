9e6e3f8f8415bd3ab43d905481e2db18207b0b3a
bbi-shadow <nguyenvanbinh270706@gmail.com>
2025-11-08 20:16:46 +0700

    udm

--- includes/header.php
+++ includes/header.php
@@ -29,40 +29,16 @@
                 <li><a href="../../views/index/contact.php">Liên hệ</a></li>
                 <li class="hd_lg"><a href="../../views/index/login.php">Đăng nhập</a></li>
             </ul>
-             <?php
-    $servername = 'localhost';
-    $username = 'root';
-    $password = '';
-    $dbname   = 'wind';
-
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-    // set charset if connected
-    if ($conn) {
-      mysqli_set_charset($conn, 'utf8mb4');
-    }
-
-    if (mysqli_connect_errno()) {
-      error_log('MySQL connect error: ' . mysqli_connect_error());
-      die('Database connection failed.'); // keep message generic for security
-    }   $_key = isset($_GET['key']) ? $_GET['key'] : ''; 
-    $_sql = "SELECT * FROM tour";
-    if (!empty($_key)) {
-      $_sql = "SELECT * From tour where ten_tour LIKE '%" . mysqli_real_escape_string($conn, $_key)."%'";
-     // $_sql = "SELECT * FROM tour WHERE name_tour LIKE '%" . mysqli_real_escape_string($conn, $_key) . "%'";
-    }
-    ?>
-    <div class="search">
-
-    <form class="search_form search" action="" method="GET">
-  
-                <input type="text" name="key" placeholder="Tìm kiếm..." value="<?php echo htmlspecialchars($_key); ?>" id="search-input">
-                <button type="submit"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-</svg>
-</button>
+            <div class="search">
+              <form action="../../views/index/tour.php" method="GET" class="search">
+        <input type="text" name="key" placeholder="Tìm kiếm...">
+        <button type="submit">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
+        </button>
     </form>
-    <button class="sangtoi" id="sangtoi">
+ <button class="sangtoi" id="sangtoi">
         <svg class="sun" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
 </svg>
@@ -71,9 +47,10 @@
 </svg>
 
       </button>
-        <div onclick="window.location.href = '/html/users.html'" class="users_avata">
-        <img src="https://i.pinimg.com/736x/ad/76/e5/ad76e544f4d3d2eca8dec9644867be85.jpg" alt="">
+        <div onclick="window.location.href = '../user/users.php'" class="users_avata">
+        <img src="https://i.pinimg.com/1200x/ce/5f/d3/ce5fd3590095d2aabe3ad6f6203dfe70.jpg" alt="">
       </div>
+            </div>
           
         </div>
         <div class="rbc_menu" id="rbc_menu">
@@ -86,20 +63,13 @@
                 <li class="hd_lg"><a href="../../views/index/login.php">Đăng nhập</a></li>
             </ul>
             <div class="rpc_search">
-              <input type="text">
-              <button>Tìm kiếm</button>
-              <div class="sangtoi2">
-                <button class="sangtoi" id="sangtoi">
-        <svg class="sun" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
-</svg>
-<svg  class="moon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
-</svg>
-
-      </button>
-              </div>
-              <div onclick="window.location.href = '../user/users.php'" class="rcp_avt"><img src="https://i.pinimg.com/1200x/ce/5f/d3/ce5fd3590095d2aabe3ad6f6203dfe70.jpg" alt=""></div>
-            </div>
+    <form action="../../views/index/tour.php" method="GET">
+        <input type="text" name="key" placeholder="Tìm kiếm...">
+        <button type="submit">Tìm kiếm</button>
+    </form>
+    <div class="rcp_avt">
+        <img src="https://i.pinimg.com/1200x/ce/5f/d3/ce5fd3590095d2aabe3ad6f6203dfe70.jpg" alt="">
+    </div>
+</div>
 </div>
   </div>
